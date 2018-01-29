55f719ed2a24dc842692747bcd6323c8395b36f2
Nikhil Patel <n.patel@berkeley.edu>
2018-01-29 12:43:38 -0800

    Improve remarks parsing with regex

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express'),
-    app = express();
+'use strict'
 
+const express = require('express');
+const app = express();
 
 const csv = require('csv-parser');
 const fs = require('fs'); 
@@ -24,40 +25,62 @@ app.get('/view', function(req, res) {
 
 
 
-loadData = ()=>{
-	json_data = []
-
-
-	my_headers = ['ent_num', 'sdn_name', 'sdn_type', 'program', 'title', 'call_sign', 'vess_type', 'tonnage', 'grt', 'vess_flag', 'vess_owner', 'remarks']
+let loadData = () => {
+	let json_data = [];
+	let csv_headers = ['ent_num', 'sdn_name', 'sdn_type', 'program', 'title', 'call_sign', 'vess_type', 'tonnage', 'grt', 'vess_flag', 'vess_owner', 'remarks']
 	// remarks_headers = new Set(['DOB', 'a.k.a.', 'POB', 'Passport', 'SSN', 'NIT', 'Cedula No.', 'D.N.I', 'Linked To:', 'R.F.C.', 'nationality', 'National ID No.', 'Additional Sanctions Information -', 'citizen', 'UK Company Number', 'Website:', 'Website', 'Aircraft Construction', 'Vessel Registration Identification', 'Gender', 'SWIFT/BIC', 'Tax ID No.', 'Email Address', 'Telephone:', 'Phone No.', 'Registration ID', 'Company Number'])
+    let remarks_matcher = /(Linked To:|nationality|DOB|a\.k\.a\.|POB|Passport|NIT|Cedula\sNo\.|SSN|D\.N\.I\.|R\.F\.C\.|Website:|Website|Vessel Registration Identification|Gender|SWIFT\/BIC|Tax ID No\.|Email Address|Telephone:|Phone No\.|Registration ID|Company Number|Aircraft Construction Number|citizen|Additional Sanctions Information \-|Aircraft Manufacture Date|Aircraft Model|Aircraft Operator|Position:|National ID No\.|Identification Number|Previous Aircraft Tail Number)\s(.*)/
+    // HANDLE ALT SOMEHOW
+
 
 	fs.createReadStream('sdn.csv')
-	.pipe(csv({headers:my_headers}))
-  	.on('data', function (data) {
-    	for (var attr in data){
-    		if(data[attr] == "-0- "){
-    			data[attr] = null;
-    		}
-    	}
-    	if(data.remarks != null){
-    		remarks_separated = data.remarks.split(";")
-    		for (var i =0; i<remarks_separated.length-1; i++){
-    			remark = remarks_separated[i]
-    			if(remark[0] == ' '){
-    				remark = remark.substr(1);
-    			}
-    			var field_name = remark.substr(0, remark.indexOf(' '))
-    			var field_value = remark.substr(remark.indexOf(' ') + 1);
-    			data[field_name] = field_value
-    		}
-    	}
-    	json_data.push(data)
-  });
+    	.pipe(csv({
+            headers: csv_headers
+        }))
+        .on('data', data => {
+            for (var attr in data) {
+        		if (data[attr] == "-0- ") {
+        			data[attr] = null;
+        		}
+        	}
+
+        	if (data.remarks != null) {
+        		let remarks_separated = data.remarks.split(";");
+
+        		for (var i = 0; i < remarks_separated.length; i++) {
+        			let remark = remarks_separated[i];
+        			let result = remarks_matcher.exec(remark);
+
+                    if (result !== null) {
+                        let field_name = result[1];
+                        let field_value = result[2];
+            			data[field_name] = field_value;
+                    }
+                    else {
+                        if (data.hasOwnProperty('notes')) {
+                            data['notes'] += '; ' + remark;
+                        }
+                        else {
+                            data['notes'] = remark;
+                        }
+                    }
+        		}
+        	}
+
+        	json_data.push(data);
+        })
+        .on('end', () => {
+            shipToDB(json_data);
+        });
 }
 
-loadData()
+loadData();
 
 
+function shipToDB(json_data) {
+    console.log(json_data[450]);
+    console.log(Object.keys(json_data).length);
+}
 
 // var sanction_schema = new Schema({
 // 	csv_id:Number, 
