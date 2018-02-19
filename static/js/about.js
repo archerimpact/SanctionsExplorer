3b0b03c82b07baa0a74e68a515b885c3d43f1ade
Nikhil Patel <n.patel@berkeley.edu>
2018-02-19 01:32:49 -0800

    Add content to, and style, the About page

--- static/js/about.js
+++ static/js/about.js
@@ -14,7 +14,12 @@ $(document).ready(() => {
 		{
 			id: 'effective',
 			question: 'How do I most effectively search?',
-			answer: '<p>If no results are being returned, try removing some words from your query.</p>'
+			answer: '<p>If no results are being returned, try removing some words from your query.</p>',
+		},
+		{
+			id: 'disclaimer',
+			question: 'Are you affiliated with the Treasury? Can I use this for compliance?',
+			answer: '<p>OFACasaurus, Archer, and C4ADS are not affiliated with the U.S. Department of Treasury, which does not endorse OFACasaurus.  Additionally, OFACasaurus, Archer, and C4ADS make no guarantee on the accuracy of the data contained in the OFACasaurus platform -- although we do our best to ensure that no data is missing, we do not recommend using OFACasaurus for compliance (at least, not without verifying using the official Treasury website).</p>',
 		}
 	];
 
