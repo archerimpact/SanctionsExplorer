c74dabbbe1de97c14a9a491e4949cf1abef0a9a6
Nikhil Patel <n.patel@berkeley.edu>
2018-02-22 00:44:43 -0800

    More cleanup and refactoring (mixed tabs and spaces messed up on vim)

--- app.js
+++ app.js
@@ -64,19 +64,19 @@ app.get('/search', function(req, res) {
 
     if (Object.keys(search_query).length !== 0) {
         Entry.find(search_query, function(err, result){
-         	if (err) {
+             if (err) {
                 res.status(400).end();
-         	}
-         	else {
-         		res.json(result);
-         	}
+             }
+             else {
+                 res.json(result);
+             }
         });
     }
 });
 
 app.get('/elasticsearch', function(req, res) {
-    // res.send('search');
-    var keywords = ["id", "ent_num", "sdn_name","sdn_type","program","title","call_sign","vess_type","tonnage","grt","vess_flag","vess_owner","remarks","linked_to","nationality","dob","aka","pob","passport","nit","cedula_no","ssn","dni","rfc","website","vessel_registration_number","gender","swift_bic","tax_id_no","email","phone","registration_id","company_number","aircraft_construction_number","citizen","additional_sanctions_info","aircraft_manufacture_date","aircraft_model","aircraft_operator","position","national_id_number","identification_number","previous_aircraft_tail_number"]
+    const keywords = ["id", "ent_num", "sdn_name","sdn_type","program","title","call_sign","vess_type","tonnage","grt","vess_flag","vess_owner","remarks","linked_to","nationality","dob","aka","pob","passport","nit","cedula_no","ssn","dni","rfc","website","vessel_registration_number","gender","swift_bic","tax_id_no","email","phone","registration_id","company_number","aircraft_construction_number","citizen","additional_sanctions_info","aircraft_manufacture_date","aircraft_model","aircraft_operator","position","national_id_number","identification_number","previous_aircraft_tail_number"]
+
     var es_query = {size: 300, from: 0};
     var search_query = {bool:{must:[]}};
 
@@ -100,114 +100,104 @@ app.get('/elasticsearch', function(req, res) {
 
    console.log(search_query);
    for (var i = 0; i < keywords.length; i++) {
-		if (req.query[keywords[i]] != null) {
-			// console.log(keywords[i]);
-			// var subquery = {match:{}}
-			// var sbq = {}
-			// sbq[keywords[i]] = {}
-			// sbq[keywords[i]].query = req.query[keywords[i]]
-			// sbq[keywords[i]].fuzziness = "AUTO"
+        if (req.query[keywords[i]] != null) {
             let match_phrase = create_match_phrase(keywords[i], req.query[keywords[i]])
-			// subquery.match = sbq
-//			console.log(sbq);
-//			console.log(subquery);
-			search_query.bool.must.push(match_phrase)
-		}
-	}
-//	console.log(search_query);
-//	console.log(search_query.bool.should);
-	es_query.query = search_query;
-//	console.log(es_query);
-   if (Object.keys(search_query).length !== 0) {
-     Entry.esSearch(es_query, function(err, results){
-     	console.log(results);
-	if(err){
-     		res.status(400).end();
-     	}
-     	else{
-		let response = [];
-		for (var i in results.hits.hits) {
-			response.push(results.hits.hits[i]['_source']);
-		}
-		console.log(response);
-     		res.json(response);
-     	}
-     });
-   }
+            search_query.bool.must.push(match_phrase)
+        }
+    }
+
+    es_query.query = search_query;
+
+    if (Object.keys(search_query).length !== 0) {
+        Entry.esSearch(es_query, (err, results) => {
+            console.log(results);
+            if (err) {
+                res.status(400).end();
+            }
+            else {
+                let response = [];
+                for (var i in results.hits.hits) {
+                    response.push(results.hits.hits[i]['_source']);
+                }
+                console.log(response);
+                res.json(response);
+            }
+        });
+    }
 });
 
 app.get('/elasticsearch/all', function(req, res){
-	var keywords = ["id", "ent_num", "sdn_name","sdn_type","program","title","call_sign","vess_type","tonnage","grt","vess_flag","vess_owner","remarks","linked_to","nationality","dob","aka","pob","passport","nit","cedula_no","ssn","dni","rfc","website","vessel_registration_number","gender","swift_bic","tax_id_no","email","phone","registration_id","company_number","aircraft_construction_number","citizen","additional_sanctions_info","aircraft_manufacture_date","aircraft_model","aircraft_operator","position","national_id_number","identification_number","previous_aircraft_tail_number"]
-	if(req.query.query){
-		var search_query = {}
-		search_query.multi_match = {}
-		search_query.multi_match.query = req.query.query;
-		search_query.fields = keywords;
+    var keywords = ["id", "ent_num", "sdn_name","sdn_type","program","title","call_sign","vess_type","tonnage","grt","vess_flag","vess_owner","remarks","linked_to","nationality","dob","aka","pob","passport","nit","cedula_no","ssn","dni","rfc","website","vessel_registration_number","gender","swift_bic","tax_id_no","email","phone","registration_id","company_number","aircraft_construction_number","citizen","additional_sanctions_info","aircraft_manufacture_date","aircraft_model","aircraft_operator","position","national_id_number","identification_number","previous_aircraft_tail_number"]
+    if(req.query.query){
+        var search_query = {}
+        search_query.multi_match = {}
+        search_query.multi_match.query = req.query.query;
+        search_query.fields = keywords;
 
-		Entry.search(search_query, function(err, results){
-			console.log(results);
+        Entry.search(search_query, function(err, results){
+            console.log(results);
 
-			if(err){
-				res.status(400).end();
-			}
-			else{
-				res.json(results.hits.hits);
-			}
-		})
+            if(err){
+                res.status(400).end();
+            }
+            else{
+                res.json(results.hits.hits);
+            }
+        })
 
-	}
-	else{
-		res.status(400).send("No parameter provided for search");
-	}
+    }
+    else{
+        res.status(400).send("No parameter provided for search");
+    }
 })
 
 app.get('/view', function(req, res) {
-   res.send('view');
+    res.send('view');
 });
 
 const entrySchema = mongoose.Schema({
-	ent_num:String,
-	sdn_name:String,
-	sdn_type:String,
-	program:String,
-	title:String,
-	call_sign:String,
-	vess_type:String,
-	tonnage:String,
-	grt:String,
-	vess_flag:String,
-	vess_owner:String,
-	remarks:String,
-	linked_to:[String],
-	nationality:[String],
-	dob:[String],
-	aka:String,
-	pob:[String],
-	passport:[String],
-	nit:[String],
-	cedula_no:String,
-	ssn:String,
-	dni:String,
-	rfc:[String],
-	website:[String],
-	vessel_registration_number:String,
-	gender:String,
-	swift_bic:[String],
-	tax_id_no:[String],
-	email:[String],
-	phone:String,
-	registration_id:[String],
-	company_number:String,
-	aircraft_construction_number:String,
-	citizen:[String],
-	additional_sanctions_info:[String],
-	aircraft_manufacture_date:String,
-	aircraft_model:[String],
-	aircraft_operator:[String],
-	position:String,
-	national_id_number:[String],
-	identification_number:[String],
-	previous_aircraft_tail_number:String
+    ent_num:String,
+    sdn_name:String,
+    sdn_type:String,
+    program:String,
+    title:String,
+    call_sign:String,
+    vess_type:String,
+    tonnage:String,
+    grt:String,
+    vess_flag:String,
+    vess_owner:String,
+    remarks:String,
+    linked_to:[String],
+    nationality:[String],
+    dob:[String],
+    aka:String,
+    pob:[String],
+    passport:[String],
+    nit:[String],
+    cedula_no:String,
+    ssn:String,
+    dni:String,
+    rfc:[String],
+    website:[String],
+    vessel_registration_number:String,
+    gender:String,
+    swift_bic:[String],
+    tax_id_no:[String],
+    email:[String],
+    phone:String,
+    registration_id:[String],
+    company_number:String,
+    aircraft_construction_number:String,
+    citizen:[String],
+    additional_sanctions_info:[String],
+    aircraft_manufacture_date:String,
+    aircraft_model:[String],
+    aircraft_operator:[String],
+    position:String,
+    national_id_number:[String],
+    identification_number:[String],
+    previous_aircraft_tail_number:String
 });
 
 entrySchema.plugin(mongoosastic);
@@ -215,9 +205,9 @@ let Entry = mongoose.model('Entry', entrySchema);
 
 
 let loadData = () => {
-	let json_data = [];
-	let csv_headers = ['ent_num', 'sdn_name', 'sdn_type', 'program', 'title', 'call_sign', 'vess_type', 'tonnage', 'grt', 'vess_flag', 'vess_owner', 'remarks']
-	// remarks_headers = new Set(['DOB', 'a.k.a.', 'POB', 'Passport', 'SSN', 'NIT', 'Cedula No.', 'D.N.I', 'Linked To:', 'R.F.C.', 'nationality', 'National ID No.', 'Additional Sanctions Information -', 'citizen', 'UK Company Number', 'Website:', 'Website', 'Aircraft Construction', 'Vessel Registration Identification', 'Gender', 'SWIFT/BIC', 'Tax ID No.', 'Email Address', 'Telephone:', 'Phone No.', 'Registration ID', 'Company Number'])
+    let json_data = [];
+    let csv_headers = ['ent_num', 'sdn_name', 'sdn_type', 'program', 'title', 'call_sign', 'vess_type', 'tonnage', 'grt', 'vess_flag', 'vess_owner', 'remarks']
+    // remarks_headers = new Set(['DOB', 'a.k.a.', 'POB', 'Passport', 'SSN', 'NIT', 'Cedula No.', 'D.N.I', 'Linked To:', 'R.F.C.', 'nationality', 'National ID No.', 'Additional Sanctions Information -', 'citizen', 'UK Company Number', 'Website:', 'Website', 'Aircraft Construction', 'Vessel Registration Identification', 'Gender', 'SWIFT/BIC', 'Tax ID No.', 'Email Address', 'Telephone:', 'Phone No.', 'Registration ID', 'Company Number'])
     let remarks_matcher = /(Linked To:|nationality|DOB|a\.k\.a\.|POB|Passport|NIT|Cedula\sNo\.|SSN|D\.N\.I\.|R\.F\.C\.|Website:|Website|Vessel Registration Identification|Gender|SWIFT\/BIC|Tax ID No\.|Email Address|Telephone:|Phone No\.|Registration ID|Company Number|Aircraft Construction Number|citizen|Additional Sanctions Information \-|Aircraft Manufacture Date|Aircraft Model|Aircraft Operator|Position:|National ID No\.|Identification Number|Previous Aircraft Tail Number)\s(.*)/
     // HANDLE ALT SOMEHOW
 
@@ -232,40 +222,40 @@ let loadData = () => {
 
     var remarks_to_fields = {}
     for (var i = 0; i<field_names.length; i++){
-    	remarks_to_fields[remarks_names[i]] = field_names[i]
+        remarks_to_fields[remarks_names[i]] = field_names[i]
     }
     // console.log(remarks_to_fields)
 
-	fs.createReadStream('sdn.csv')
-    	.pipe(csv({
+    fs.createReadStream('sdn.csv')
+        .pipe(csv({
             headers: csv_headers
         }))
         .on('data', data => {
             for (var attr in data) {
-        		if (data[attr] == "-0- ") {
-        			data[attr] = null;
-        		}
-        	}
+                if (data[attr] == "-0- ") {
+                    data[attr] = null;
+                }
+            }
 
-        	if (data.remarks != null) {
-        		// console.log(data.remarks)
-        		let remarks_separated = data.remarks.split(";");
+            if (data.remarks != null) {
+                // console.log(data.remarks)
+                let remarks_separated = data.remarks.split(";");
 
 
-        		for (var i = 0; i < remarks_separated.length; i++) {
+                for (var i = 0; i < remarks_separated.length; i++) {
 
-        			var is_alt = false;
+                    var is_alt = false;
 
-        			let remark = remarks_separated[i]
+                    let remark = remarks_separated[i]
 
-        			if(remarks_separated[i].substring(0,4) === " alt"){
-        				is_alt = true
-        				let remark = remarks_separated[i].substring(6, remarks_separated[i].length);
-        				// console.log(remark)
-        			}
+                    if(remarks_separated[i].substring(0,4) === " alt"){
+                        is_alt = true
+                        let remark = remarks_separated[i].substring(6, remarks_separated[i].length);
+                        // console.log(remark)
+                    }
 
-        			// let remark = remarks_separated[i];
-        			let result = remarks_matcher.exec(remark);
+                    // let remark = remarks_separated[i];
+                    let result = remarks_matcher.exec(remark);
 
                     if (result !== null) {
 
@@ -275,24 +265,24 @@ let loadData = () => {
                         field_set.add(field_name)
 
                         if(is_alt){
-                        	// console.log("ALT");
-                        	alt_set.add(field_name);
+                            // console.log("ALT");
+                            alt_set.add(field_name);
                         }
 
 
                         if(list_fields.has(field_name)){
-                        	// console.log(field_name)
-                        	// console.log("Array add for "+field_name);
-                        	if(data[field_name] == null){
-                        		data[field_name] = [field_value]
-                        	}
-                        	else{
-                        		data[field_name].push(field_value)
-                        	}
+                            // console.log(field_name)
+                            // console.log("Array add for "+field_name);
+                            if(data[field_name] == null){
+                                data[field_name] = [field_value]
+                            }
+                            else{
+                                data[field_name].push(field_value)
+                            }
                         }
                         else{
-							data[field_name] = field_value;
-						}
+                            data[field_name] = field_value;
+                        }
 
                     }
                     else {
@@ -303,13 +293,13 @@ let loadData = () => {
                             data['notes'] = remark;
                         }
                     }
-        		}
-        		// console.log(data)
-        	}
-        	json_data.push(data);
+                }
+                // console.log(data)
+            }
+            json_data.push(data);
         })
         .on('end', () => {
-        	// console.log(json_data[10])
+            // console.log(json_data[10])
             shipToDB(json_data);
         });
 }
@@ -319,39 +309,39 @@ let loadData = () => {
 var j =0;
 
 // Entry.search({query_string:
-// 	{
-// 		query:{
-// 			fuzzy:{
-// 				sdn_name:"arocarribean airlines"
-// 			}
-// 		}
-// 	}
-// 	}, function(err, results){
-// 		console.log(results);
+//     {
+//         query:{
+//             fuzzy:{
+//                 sdn_name:"arocarribean airlines"
+//             }
+//         }
+//     }
+//     }, function(err, results){
+//         console.log(results);
 // });
 
 
 function shipToDB(json_data) {
-	// NOTE: mongo times out when trying to save all documents at once
+    // NOTE: mongo times out when trying to save all documents at once
 
 
     console.log(json_data[450]);
     console.log(Object.keys(json_data).length);
 
     for(var i =6000; i< json_data.length; i++){
-    	const entry = new Entry(json_data[i])
-	//sleep.msleep();
-	console.log("Doing doc "+i);
-    	entry.save(function(err, data){
-		//console.log("Saving");
-    		if(err){
-    			console.log(err)
-    		}
-		entry.on('es-indexed', function(err, res){
-			console.log("We indexed "+j);
-			j++;
-		});
-    	});
+        const entry = new Entry(json_data[i])
+    //sleep.msleep();
+    console.log("Doing doc "+i);
+        entry.save(function(err, data){
+        //console.log("Saving");
+            if(err){
+                console.log(err)
+            }
+        entry.on('es-indexed', function(err, res){
+            console.log("We indexed "+j);
+            j++;
+        });
+        });
     }
 
 }
