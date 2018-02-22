26e35204d5f293ad96af5a59f521dba4bcab7879
Nikhil Patel <n.patel@berkeley.edu>
2018-02-22 12:43:38 -0800

    Generalize DB shipper (part 1)

--- press_release_parser.js
+++ press_release_parser.js
@@ -7,38 +7,48 @@ const async = require('async');
 mongoose.connect('mongodb://localhost/press-releases');
 
 const prSchema = mongoose.Schema({
-	title:String,
-	link:String,
-	date:String,
-	content:String
+    title:String,
+    link:String,
+    date:String,
+    content:String
 });
 
 prSchema.plugin(mongoosastic);
-
 let PR = mongoose.model('PR', prSchema);
 
-let stream = PR.synchronize();
-var count = 0;
 
-stream.on('data', (err, doc) => count++);
-stream.on('close', () => console.log('Indexed ' + count + ' document'));
-stream.on('error', (err) => console.log(err));
+let mongooseModel = PR;
+shipFromFile('press_release_json.txt', mongooseModel);
+sync(mongooseModel);
 
 
+function sync(model) {
+    let stream = model.synchronize();
+    var count = 0;
 
-fs.readFile('press_release_json.txt', 'utf8', (err, data) => {
-	let json = JSON.parse(data);
-	shipToDB(json);
-});
+    stream.on('data', (err, doc) => count++);
+    stream.on('close', () => console.log('Indexed ' + count + ' document'));
+    stream.on('error', (err) => console.log(err));
+}
 
-function shipToDB(json_data) {
-	console.log("# keys: " + Object.keys(json_data).length);
 
-	for (var i = 0; i < json_data.length; i++) {
-		console.log('Shipping document ' + i);
-                let newPR = new PR(json_data[i]);
-                newPR.save((err) => {
-                    if (err) { console.log(err) }
-                });
-        }
+function shipFromFile(filename, model) {
+    fs.readFile(filename, 'utf8', (err, data) => {
+        let json = JSON.parse(data);
+        shipToDB(json, model);
+    });
+}
+
+function shipToDB(json_data, model) {
+    console.log("# keys: " + Object.keys(json_data).length);
+
+    for (var i = 0; i < json_data.length; i++) {
+        console.log('Shipping document ' + i);
+        let newEntry = new model(json_data[i]);
+        newEntry.save((err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
 }
