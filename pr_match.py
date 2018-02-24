d33594e945f29cd33a3ecd444d7f87518753db9c
Ryan Hayes <r.hayes@berkeley.edu>
2018-02-24 15:43:32 -0800

    prelim pr match script

---
+++ pr_match.py
@@ -0,0 +1,21 @@
+import os
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import requests
+
+wb = load_workbook(filename='sdn.xlsx')
+ws = wb[wb.get_sheet_names()[0]]
+
+url = "localhost:9200/prs/_search"
+headers = {'Content-Type: application/json'}
+data = {"query": { "match": {"content": {"query": "", "fuzziness": "AUTO"}
+
+for row in ws.rows:
+	val = row[1].value
+	print(val)
+	if val != None:
+		data["query"]["match"]["content"]["query"] = val
+		result = requests.get(url, headers=headers, data=data)
+		if result.status_code == 200:
+			print(result.content)
+		
