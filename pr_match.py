d3a093788e290c81fb5a2d9a1e34f9d48f7ae2b6
Ryan Hayes <r.hayes@berkeley.edu>
2018-02-26 22:24:17 +0000

    matches xlsx

--- pr_match.py
+++ pr_match.py
@@ -1,20 +1,35 @@
 import os
 from openpyxl import load_workbook
+from openpyxl import Workbook
 import requests
 
-url = "localhost:9200/prs/_search"
+url_template = "http://localhost:8080/search/press-releases?query="
 headers = {'Content-Type': 'application/json'}
-data = {"query": { "match": {"content": {"query": "", "fuzziness": "AUTO"}}}}
+data = {"query": "Colima"}
 
 wb = load_workbook(filename='sdn.xlsx')
-ws = wb[wb.get_sheet_names()[0]]
+ws = wb.active
+
+new_wb = Workbook()
+new_ws = wb.active
+rownum = 1
 
 for row in ws.rows:
 	val = row[1].value
+	print(type(val))
 	print(val)
-	if val != None:
-		data["query"]["match"]["content"]["query"] = val
+	if val != None and isinstance(val, unicode):
+		url = url_template + val
 		result = requests.get(url, headers=headers, data=data)
 		if result.status_code == 200:
-			print(result.content)
-		
+			for entry in result.json()["response"]:
+				new_ws.cell(row=rownum, column=1).value = val
+				new_ws.cell(row=rownum, column=2).value = entry["link"]
+				print(entry["link"])
+		else:
+			print("error")
+
+new_wb.save("prelim_matches.xlsx")
+#result = requests.get(url)
+#print(result.status_code)
+#print(result.json()["response"][0].keys())		
