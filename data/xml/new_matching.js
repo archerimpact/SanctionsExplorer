79afae39fb1e59d71e6d693829b91bdffa5e9de2
Ryan Hayes <r.hayes@berkeley.edu>
2018-03-07 21:42:28 +0000

    entries found! put into json file

--- data/xml/new_matching.js
+++ data/xml/new_matching.js
@@ -12,20 +12,42 @@ reqs[0] = [];
 var i = 0;
 var x = 0;
 var j = 0;
+var responses = {};
 
 function runSearches() {
 	if (i == j) {
-		return;
+		let json = JSON.stringify(entries);
+		fs.writeFile("entries.json", json, 'utf8', function(err, data) {
+			console.log("write complete!");
+			return;
+		})
 	} else {
 		client.msearch({
-			maxConcurrentSearches:10,
 			body:reqs[j]
 		}, function(err, responses) {
 			if (err) {
 				console.log("err");
 			}	
 			console.log('finished pass: ' + j.toString());
-			console.log(responses);
+			//console.log(responses);
+			var q = 0;
+			responses.responses.forEach(function(response) {
+				//console.log(response.hits.hits)
+				let name = reqs[j][q+1].query.query_string.query;
+				//console.log(name);
+				if (response.hits == null) {
+					//console.log(response);
+				} else {
+					response.hits.hits.forEach(function(hit) {
+						if (hit._source.primary_display_name === name) {
+							entries[name].id = hit._id;
+					//	console.log("match");
+						}
+					})
+				}
+				//entries[reqs[j][q + 1].query.query_string.query].id = response.hits.hits[0]._id;
+				q = q + 2;
+			})
 			j = j + 1;
 			runSearches();
 		});
@@ -36,9 +58,9 @@ function getReqs(entry_lines) {
   entry_lines.forEach(function(line) {
 	let line_split = line.split(" | ");
 	if (entries[line_split[0]] == null) {
-		entries[line_split[0]] = [];
+		entries[line_split[0]] = {data: []};
 	}
-	entries[line_split[0]].push({link: line_split[1], date: line_split[2], title: line_split[3]});
+	entries[line_split[0]].data.push({link: line_split[1], date: line_split[2], title: line_split[3]});
 	let search_query = {
 		search: {
 			index: 'sdn',
@@ -46,11 +68,11 @@ function getReqs(entry_lines) {
 		}
 	}
   });
-
+  console.log(Object.keys(entries).length)
   Object.keys(entries).forEach(function(entry) {
   	let q = { query: { query_string: { query: entry}}};
 	let search_index = { index: 'sdn', type: 'entry' }
-	if (x < 400) {
+	if (x < 10) { 
 		reqs[i].push(search_index);
 		reqs[i].push(q);
 		x = x + 1;
