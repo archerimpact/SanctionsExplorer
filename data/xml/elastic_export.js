975f5c5598fa6b1be8e3c0b60a6f3776b97ee29d
Nikhil Patel <n.patel@berkeley.edu>
2018-03-06 23:09:38 -0800

    Add additional fields (extract features, document headers list) for searchability and rendering

--- data/xml/elastic_export.js
+++ data/xml/elastic_export.js
@@ -34,15 +34,92 @@ for (var i = 0; i < data.length; i++) {
 
     data[i].identity.aliases.forEach(alias => {
         data[i].all_display_names.push(alias.display_name);
+        if (alias.date_period !== null) {
+            console.log('ERROR: OFAC has started associating date periods with aliases.  These will not be rendered.');
+        }
+        delete alias.date_period;
+
+        // simply for decreasing complexity within the rendering template. TODO should probably be moved to XML parser?
+        if (alias.is_low_quality) {
+            alias.strength = 'weak';
+        }
+        else {
+            alias.strength = 'strong';
+        }
     });
 
     Object.keys(data[i].features).forEach(f_key => {
         let formatted_key = f_key.split(' ').map(w => w.toLowerCase()).join('_');    // e.g. 'Citizenship Country' -> 'citizenship_country'
-        data[i][formatted_key] = data[i].features[f_key];
+
+        let combined_info = [];
+
+        data[i].features[f_key].forEach(entry => {
+            if (entry.details) {
+                if (Array.isArray(entry.details)) {
+                    entry.details.forEach(d => {
+                        combined_info.push(d);
+                    });
+                }
+                else {
+                    combined_info.push(entry.details);
+                }
+            }
+
+            if (entry.dates) {
+                entry.dates.forEach(d => {
+                    combined_info.push(d);
+                });
+            }
+
+            if (entry.locations) {
+                let locs = [];
+                entry.locations.forEach(loc => {
+                    combined_info.push(loc['COMBINED']);
+                });
+            }
+        });
+
+        data[i][formatted_key] = combined_info;
     });
 
     data[i].documents.forEach(doc => {
+        // for searchability
         data[i].doc_id_numbers.push(doc.id_number);
+         // for website display
+        if (doc.validity != 'Valid' && doc.validity != 'Fraudulent') {
+            console.log('WARNING: An invalid document status appeared (normally Valid or Fraudulent): ' + doc.validity);
+        }
+        headers = []
+
+        // TODO the nullification on 'None' logic should be moved to the XML parser.
+        if (doc.issued_by != 'None') {
+            headers.push('issued_by');
+        }
+        else {
+            doc.issued_by = null;
+        }
+
+        if (doc.issued_in != 'None') {
+            headers.push('issued_in');
+        }
+        else {
+            doc.issued_in = null;
+        }
+
+        if (doc.issuing_authority != 'None') {
+            headers.push('issuing_authority');
+        }
+        else {
+            doc.issuing_authority = null;
+        }
+
+        Object.keys(doc.relevant_dates).forEach(date => {
+            doc[date] = doc.relevant_dates[date];
+            headers.push(date);
+        });
+
+        data[i].document_headers = headers
+
     });
 
     let es_index_statement = {
