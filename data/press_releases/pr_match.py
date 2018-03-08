939635d142dfc1fe2df4c52fef7e44a8318c6956
Ryan Hayes <r.hayes@berkeley.edu>
2018-03-08 03:40:06 +0000

    pushing script

--- data/press_releases/pr_match.py
+++ data/press_releases/pr_match.py
@@ -4,9 +4,9 @@ from openpyxl import Workbook
 import requests
 import urllib
 
-url_template = "http://localhost:8080/search/press-releases"
+url_template = "http://localhost:9200/pr/pr/_search"
 headers = {'Content-Type': 'application/json'}
-data = {"query": {"match": {"query": "", "fuziness": "0", "operator": "and"}}}
+data = {"query": {"match": {"context" :{"query": "", "operator": "and"}}}}
 
 wb = load_workbook(filename='../xml/sdn.xlsx')
 ws = wb.active
@@ -16,23 +16,26 @@ writefile = open("match_data.txt", "w")
 #new_wb = Workbook()
 #new_ws = new_wb.active
 rownum = 1
-
+x = 0
 for row in ws.rows:
 	val = row[1].value
-	print(val)
+	#print(val)
 	if val != None and isinstance(val, unicode):
 		url = url_template + urllib.quote_plus(val).replace("+", "%20")
 		#print(url)
-		data["query"]["match"]["query"] = val;
+		data["query"]["match"]["context"]["query"] = urllib.quote_plus(val).replace("+", "%20");
 		#print(data)
-		result = requests.get(url_template, params=data)
+		result = requests.get(url_template, params=data["query"])
+                if (x == 0):
+                    print result.json().keys()
+                    x += 1
 		if result.status_code == 200:
 			for entry in result.json()["response"]:
 				#new_ws.cell(row=rownum, column=1).value = val
 				#new_ws.cell(row=rownum, column=2).value = entry["link"]
                 		#new_ws.cell(row=rownum, column=3).value = entry["date"]
                 		#new_ws.cell(row=rownum, column=4).value = entry["title"]
-				print(entry["link"])
+				#print(entry["link"])
 				writefile.write(val + " | " + entry["link"] + " | " + entry["date"] + " | ")
 				writefile.write(entry["title"].encode("utf-8"))
 				writefile.write("\n")
