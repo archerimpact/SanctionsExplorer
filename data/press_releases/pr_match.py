65f685f2e0d08c2bf97be1dd0f2f0a46f86ccba1
Ryan Hayes <r.hayes@berkeley.edu>
2018-03-08 00:34:30 +0000

    fresh set of matches

--- data/press_releases/pr_match.py
+++ data/press_releases/pr_match.py
@@ -4,15 +4,17 @@ from openpyxl import Workbook
 import requests
 import urllib
 
-url_template = "http://localhost:8080/search/press-releases?query="
+url_template = "http://localhost:8080/search/press-releases?operator=and&query="
 headers = {'Content-Type': 'application/json'}
 data = {"query": "Colima"}
 
 wb = load_workbook(filename='../xml/sdn.xlsx')
 ws = wb.active
 
-new_wb = Workbook()
-new_ws = new_wb.active
+
+writefile = open("match_data.txt", "w")
+#new_wb = Workbook()
+#new_ws = new_wb.active
 rownum = 1
 
 for row in ws.rows:
@@ -24,16 +26,21 @@ for row in ws.rows:
 		result = requests.get(url)
 		if result.status_code == 200:
 			for entry in result.json()["response"]:
-				new_ws.cell(row=rownum, column=1).value = val
-				new_ws.cell(row=rownum, column=2).value = entry["link"]
-                new_ws.cell(row=rownum, column=3).value = entry["date"]
-                new_ws.cell(row=rownum, column=4).value = entry["title"]
-				print(entry["link"])
+				#new_ws.cell(row=rownum, column=1).value = val
+				#new_ws.cell(row=rownum, column=2).value = entry["link"]
+                		#new_ws.cell(row=rownum, column=3).value = entry["date"]
+                		#new_ws.cell(row=rownum, column=4).value = entry["title"]
+				#print(entry["link"])
+				writefile.write(val + " | " + entry["link"] + " | " + entry["date"] + " | ")
+				writefile.write(entry["title"].encode("utf-8"))
+				writefile.write("\n")
 				rownum += 1
 		else:
 			print(result.content)
 
-new_wb.save("prelim_matches.xlsx")
+writefile.close()
+
+#new_wb.save("prelim_matches.xlsx")
 #result = requests.get(url)
 #print(result.status_code)
 #print(result.json()["response"][0].keys())		
