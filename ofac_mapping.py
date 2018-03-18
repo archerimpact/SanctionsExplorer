d48f5e58786c091122cd77019001e911abc108cb
Amit Talreja <atalreja@berkeley.edu>
2018-03-17 21:58:14 -0700

    removing commented lines in ofac name scraper

--- ofac_mapping.py
+++ ofac_mapping.py
@@ -23,27 +23,6 @@ error_text = "An error has occured."
 
 tup_list = []
 
-# def scrape_and_extract(start_num, end_num):
-# 	i = start_num
-# 	while (i < end_num):
-# 		url = base_url.format(i)
-# 		result = requests.get(url)
-# 		if result.status_code == 200:
-# 			c = result.content
-# 			soup = BeautifulSoup(c, 'lxml')
-# 			if update_mode:
-# 				h4 = soup.find_all('h4')
-# 				if h4 is not None and h4.text == error_text:
-# 					return
-# 			page_type = 
-# 		# 	first_name = soup.find(id=first_name_id).text
-# 		# 	last_name = soup.find(id=last_name_id).text
-# 		# 	tup_list.append((first_name, last_name, i))
-# 		print(url)
-
-# 		i += 1
-
-
 def is_type(soup, match_string):
 	match = soup.find(id=match_string)
 	return match is not None
