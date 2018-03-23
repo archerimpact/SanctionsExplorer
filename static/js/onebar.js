a51986ac643b02626a311d01d7870f2b1618b9d7
Nikhil Patel <n.patel@berkeley.edu>
2018-03-23 02:46:03 -0700

    Add search/advanced buttons to landing page

--- static/js/onebar.js
+++ static/js/onebar.js
@@ -2,18 +2,33 @@
 
 $(document).ready(() => {
 
-    $('#one-search-input').on('submit', event => {
+    let redirect = path => window.location.href = window.location.protocol + '//' + window.location.host + '/sdn' + path;
+    let submit = () => $('#one-search-input').trigger('submit');
+
+    $('#one-search-input').on('submit', function(event) {
         event.preventDefault();
         let input = $('#one-search-input').val();
-        window.location.href = window.location.protocol + '//' + window.location.host + '/sdn/?searchall=' + input;     // TODO escape things properly.
+        if (input) {
+            redirect('/?searchall=' + input);     // TODO escape things properly.
+        }
+        else {
+            redirect('/');
+        }
     });
 
     $('#one-search-input').keydown(function(event) {
         if (event.keyCode == 13) {
             event.preventDefault();
-            $('#one-search-input').submit();
+            submit();
         }
     });
 
+    $('#search-button').click(event => {
+        submit();
+    });
+
+    $('#advanced-button').click(event => {
+        redirect('/');
+    });
 
 });
