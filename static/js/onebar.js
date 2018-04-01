cb6e25ca78d03985c42deb7cd65e7a817ab9739f
Nikhil Patel <n.patel@berkeley.edu>
2018-03-31 21:32:20 -0700

    Improve markup/CSS for splash page; reorder columns on mobile for About/Feedback

--- static/js/onebar.js
+++ static/js/onebar.js
@@ -19,4 +19,22 @@ $(document).ready(() => {
     });
     $('#search-button').click(event => submit());
     $('#advanced-button').click(event => redirect(''));
+
+
+
+    center_page();
+    $(window).resize(center_page);
 });
+
+function center_page() {
+    let top_bound    = $('#info-group')[0].getBoundingClientRect().bottom;
+    let bottom_bound = $('#partner-logo-group')[0].getBoundingClientRect().top;
+    let elem_height  = $('#center-group').height();
+    let empty_mid = (bottom_bound - top_bound) / 2;
+    let mid = top_bound + empty_mid - (elem_height / 2);
+
+    let bias = 20;
+
+    // slightly less than 2 to make it a bit higher than center
+    $('#center-group').css('top', mid - 50 + 'px');
+}
