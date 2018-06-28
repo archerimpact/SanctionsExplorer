439751a802b1ecdf065551e48d3021f291ca929a
Nikhil Patel <n.patel@berkeley.edu>
2018-06-28 16:12:11 -0700

    Add gtag counter

--- static/js/sdn.js
+++ static/js/sdn.js
@@ -71,6 +71,12 @@ const EMPTY_SELECT  = 'select-field';
 const FILTER_SUMMARY_LENGTH = 12;       // used for truncation in search.js
 
 $(document).ready(() => {
+    let counter = 0;
+    setInterval(() => {
+        counter += 10;
+        console.log(counter);
+        gtag('event', 'pageLoadedTimer', { event_label: counter, value: counter })
+    }, 10000)
     window.card = get_template('#card-template');
     window.searchRow = get_template('#search-row-template');
     window.searchRowID = 0;
