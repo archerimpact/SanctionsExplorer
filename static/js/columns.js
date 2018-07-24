ad90867b6011031f805fac250fdb970abb99b81d
Nikhil Patel <n.patel@berkeley.edu>
2018-07-24 18:43:51 +0100

    Add ArcherViz links

--- static/js/columns.js
+++ static/js/columns.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     // Set heights of divs to ensure proper scrolling behavior
     let resize_col = () => {
         if ($('body').width() >= 768) {
-            $('.page-col').innerHeight($(window).height() - $('nav').outerHeight() - 1);
+            $('.page-col').innerHeight($(window).height() - $('nav').outerHeight() - $('#update-bar').outerHeight() - 2);
         }
     };
     resize_col();
